f774a28daa6ab5d8369acca0b3e82f386be3b56e
Babish <babish.shrestha@cgnet.com.np>
2023-07-09 22:18:50 +0545

    add: models for task(todo)  and user

---
+++ lib/core/models/task_model.dart
@@ -0,0 +1,53 @@
+import 'dart:convert';
+
+class Task {
+  final int? id;
+  final String? title;
+  final String? desc;
+  final int? isCompleted;
+  final String? date;
+  final String? startTime;
+  final String? endTime;
+  final int? remind;
+  final String? repeat;
+
+  Task({
+    this.id,
+    this.title,
+    this.desc,
+    this.isCompleted,
+    this.date,
+    this.startTime,
+    this.endTime,
+    this.remind,
+    this.repeat,
+  });
+
+  factory Task.fromJson(String str) => Task.fromMap(json.decode(str));
+
+  String toJson() => json.encode(toMap());
+
+  factory Task.fromMap(Map<String, dynamic> json) => Task(
+        id: json["id"],
+        title: json["title"],
+        desc: json["desc"],
+        isCompleted: json["isCompleted"],
+        date: json["date"],
+        startTime: json["startTime"],
+        endTime: json["endTime"],
+        remind: json["remind"],
+        repeat: json["repeat"],
+      );
+
+  Map<String, dynamic> toMap() => {
+        "id": id,
+        "title": title,
+        "desc": desc,
+        "isCompleted": isCompleted,
+        "date": date,
+        "startTime": startTime,
+        "endTime": endTime,
+        "remind": remind,
+        "repeat": repeat,
+      };
+}
