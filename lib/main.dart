bce4e883cd5550e0c4181d095d4f617f9cf8d72f
Babish <babish.shrestha@cgnet.com.np>
2023-07-09 22:17:20 +0545

    add: added dynamic color scheme light and dark

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -11,6 +12,14 @@ void main() {
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
+  static final defaultLightColorScheme = ColorScheme.fromSwatch(
+    primarySwatch: Colors.blue,
+  );
+
+  static final defaultDarkColorScheme = ColorScheme.fromSwatch(
+    primarySwatch: Colors.blue,
+    brightness: Brightness.dark,
+  );
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
@@ -19,14 +28,27 @@ class MainApp extends StatelessWidget {
         minTextAdapt: true,
         useInheritedMediaQuery: true,
         builder: (context, child) {
-          return MaterialApp(
-            title: 'Todo Riverpod',
-            theme: ThemeData(
+          return DynamicColorBuilder(
+              builder: (lightColorScheme, darkColorScheme) {
+            return MaterialApp(
+              title: 'Todo Riverpod',
+              theme: ThemeData(
                 scaffoldBackgroundColor: AppConst.kBkDark,
-                colorScheme: ColorScheme.fromSeed(seedColor: AppConst.kYellow)),
-            debugShowCheckedModeBanner: false,
-            home: const HomePage(),
-          );
+                // colorScheme: ColorScheme.fromSeed(
+                //   seedColor: AppConst.kYellow,
+                // ),
+                colorScheme: lightColorScheme ?? defaultLightColorScheme,
+                useMaterial3: true,
+              ),
+              darkTheme: ThemeData(
+                colorScheme: darkColorScheme ?? defaultDarkColorScheme,
+                scaffoldBackgroundColor: AppConst.kBkDark,
+                useMaterial3: true,
+              ),
+              debugShowCheckedModeBanner: false,
+              home: const HomePage(),
+            );
+          });
         });
   }
 }
