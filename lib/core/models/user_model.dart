72fd099908aa60b9153e164118e86faf5a3eac10
Babish <babish.shrestha@cgnet.com.np>
2023-07-09 22:18:50 +0545

    add: models for task(todo)  and user

---
+++ lib/core/models/user_model.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+
+class User {
+    final int id;
+    final String isVerified;
+
+    User({
+        required this.id,
+        required this.isVerified,
+    });
+
+    factory User.fromJson(String str) => User.fromMap(json.decode(str));
+
+    String toJson() => json.encode(toMap());
+
+    factory User.fromMap(Map<String, dynamic> json) => User(
+        id: json["id"],
+        isVerified: json["isVerified"],
+    );
+
+    Map<String, dynamic> toMap() => {
+        "id": id,
+        "isVerified": isVerified,
+    };
+}
