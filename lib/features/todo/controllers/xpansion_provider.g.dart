49f2ef66d4e4f46f7c8b3da5c93d5597191019de
Babish <babish.shrestha@cgnet.com.np>
2023-07-09 22:19:14 +0545

    add: created expansion state using riverpod

---
+++ lib/features/todo/controllers/xpansion_provider.g.dart
@@ -0,0 +1,41 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'xpansion_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$xpansionStateHash() => r'82909a2088e29c867c184060bc74e293f4108f35';
+
+/// See also [XpansionState].
+@ProviderFor(XpansionState)
+final xpansionStateProvider =
+    AutoDisposeNotifierProvider<XpansionState, bool>.internal(
+  XpansionState.new,
+  name: r'xpansionStateProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$xpansionStateHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$XpansionState = AutoDisposeNotifier<bool>;
+String _$xpansionState0Hash() => r'05c43f4f3f068854b44d44fa99ae7fe74e4bcf9f';
+
+/// See also [XpansionState0].
+@ProviderFor(XpansionState0)
+final xpansionState0Provider =
+    AutoDisposeNotifierProvider<XpansionState0, dynamic>.internal(
+  XpansionState0.new,
+  name: r'xpansionState0Provider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$xpansionState0Hash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$XpansionState0 = AutoDisposeNotifier<dynamic>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
