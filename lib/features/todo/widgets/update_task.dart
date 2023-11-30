399ffd4789892db2280dfd8b9133a4267cc7c2f8
Babish Shrestha <babish.shrestha@cgnet.com.np>
2023-11-30 15:41:47 +0545

    add: cubit to add page, update task. fix: issue for clearSelectedDate&Time

--- lib/features/todo/widgets/update_task.dart
+++ lib/features/todo/widgets/update_task.dart
@@ -4,24 +4,22 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_datetime_picker_plus/flutter_datetime_picker_plus.dart'
     as picker;
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:todo_riverpod/core/utils/constants.dart';
 import 'package:todo_riverpod/core/widgets/core_widgets.dart';
 import 'package:todo_riverpod/features/todo/bloc/date_cubit.dart';
 import 'package:todo_riverpod/features/todo/bloc/todo_cubit.dart';
 
 import '../../../core/models/task_model.dart';
-import '../pages/homepage.dart';
 
-class UpdateTask extends ConsumerStatefulWidget {
+class UpdateTask extends StatefulWidget {
   final Task task;
   const UpdateTask({required this.task, super.key});
 
   @override
-  ConsumerState<UpdateTask> createState() => _AddPageState();
+  State<UpdateTask> createState() => _AddPageState();
 }
 
-class _AddPageState extends ConsumerState<UpdateTask> {
+class _AddPageState extends State<UpdateTask> {
   final TextEditingController titleController = TextEditingController();
   final TextEditingController descriptionController = TextEditingController();
   late DateCubit dateCubit;
@@ -84,80 +82,86 @@ class _AddPageState extends ConsumerState<UpdateTask> {
               HeightSpacer(
                 spaceHeight: 20.h,
               ),
-              CustomOutlineButton(
-                borderColor: AppConst.kLight,
-                height: 52.h,
-                bgColor: AppConst.kBlueLight,
-                text: dateCubit.state.isEmpty ? 'Set Date' : dateCubit.state,
-                width: AppConst.kWidth,
-                onPressed: () {
-                  picker.DatePicker.showDatePicker(context,
-                      showTitleActions: true,
-                      minTime: DateTime.now(),
-                      maxTime:
-                          DateTime.now().add(const Duration(days: 365 * 2)),
-                      theme: const picker.DatePickerTheme(
-                          headerColor: Colors.white,
-                          itemStyle: TextStyle(
-                              color: AppConst.kBlueLight,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 18),
-                          doneStyle:
-                              TextStyle(color: AppConst.kGreen, fontSize: 16)),
-                      onChanged: (date) {
-                    dateCubit.setDate(date.toString().substring(0, 10));
-                  }, onConfirm: (date) {
-                    dateCubit.setDate(date.toString().substring(0, 10));
-                  }, currentTime: DateTime.now(), locale: picker.LocaleType.en);
-                },
-              ),
+              BlocBuilder<DateCubit, String>(builder: (context, state) {
+                return CustomOutlineButton(
+                  borderColor: AppConst.kLight,
+                  height: 52.h,
+                  bgColor: AppConst.kBlueLight,
+                  text: state.isEmpty ? 'Set Date' : state,
+                  width: AppConst.kWidth,
+                  onPressed: () {
+                    picker.DatePicker.showDatePicker(context,
+                        showTitleActions: true,
+                        minTime: DateTime.now(),
+                        maxTime:
+                            DateTime.now().add(const Duration(days: 365 * 2)),
+                        theme: const picker.DatePickerTheme(
+                            headerColor: Colors.white,
+                            itemStyle: TextStyle(
+                                color: AppConst.kBlueLight,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18),
+                            doneStyle: TextStyle(
+                                color: AppConst.kGreen,
+                                fontSize: 16)), onChanged: (date) {
+                      dateCubit.setDate(date.toString().substring(0, 10));
+                    }, onConfirm: (date) {
+                      dateCubit.setDate(date.toString().substring(0, 10));
+                    },
+                        currentTime: DateTime.now(),
+                        locale: picker.LocaleType.en);
+                  },
+                );
+              }),
               HeightSpacer(
                 spaceHeight: 20.h,
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  CustomOutlineButton(
-                    borderColor: AppConst.kLight,
-                    height: 52.h,
-                    bgColor: AppConst.kBlueLight,
-                    text: startTimeCubit.state.isEmpty
-                        ? 'Start Time'
-                        : startTimeCubit.state,
-                    width: AppConst.kWidth * 0.4,
-                    onPressed: () {
-                      picker.DatePicker.showTimePicker(context,
-                          onChanged: (date) {
-                            startTimeCubit
-                                .setStart(date.toString().substring(10, 16));
-                          },
-                          showTitleActions: true,
-                          onConfirm: (date) {
-                            startTimeCubit
-                                .setStart(date.toString().substring(10, 16));
-                          },
-                          locale: picker.LocaleType.en);
-                    },
-                  ),
-                  CustomOutlineButton(
-                    borderColor: AppConst.kLight,
-                    height: 52.h,
-                    bgColor: AppConst.kBlueLight,
-                    text: endTimeCubit.state.isEmpty
-                        ? 'End Time'
-                        : endTimeCubit.state,
-                    width: AppConst.kWidth * 0.4,
-                    onPressed: () {
-                      picker.DatePicker.showTimePicker(context,
-                          showTitleActions: true, onChanged: (date) {
-                        endTimeCubit
-                            .setFinish(date.toString().substring(10, 16));
-                      }, onConfirm: (date) {
-                        endTimeCubit
-                            .setFinish(date.toString().substring(10, 16));
-                      }, locale: picker.LocaleType.en);
-                    },
-                  ),
+                  BlocBuilder<StartTimeCubit, String>(
+                      builder: (context, state) {
+                    return CustomOutlineButton(
+                      borderColor: AppConst.kLight,
+                      height: 52.h,
+                      bgColor: AppConst.kBlueLight,
+                      text: state.isEmpty ? 'Start Time' : state,
+                      width: AppConst.kWidth * 0.4,
+                      onPressed: () {
+                        picker.DatePicker.showTimePicker(context,
+                            onChanged: (date) {
+                              startTimeCubit
+                                  .setStart(date.toString().substring(10, 16));
+                            },
+                            showTitleActions: true,
+                            onConfirm: (date) {
+                              startTimeCubit
+                                  .setStart(date.toString().substring(10, 16));
+                            },
+                            locale: picker.LocaleType.en);
+                      },
+                    );
+                  }),
+                  BlocBuilder<FinishTimeCubit, String>(
+                      builder: (context, state) {
+                    return CustomOutlineButton(
+                      borderColor: AppConst.kLight,
+                      height: 52.h,
+                      bgColor: AppConst.kBlueLight,
+                      text: state.isEmpty ? 'End Time' : state,
+                      width: AppConst.kWidth * 0.4,
+                      onPressed: () {
+                        picker.DatePicker.showTimePicker(context,
+                            showTitleActions: true, onChanged: (date) {
+                          endTimeCubit
+                              .setFinish(date.toString().substring(10, 16));
+                        }, onConfirm: (date) {
+                          endTimeCubit
+                              .setFinish(date.toString().substring(10, 16));
+                        }, locale: picker.LocaleType.en);
+                      },
+                    );
+                  }),
                 ],
               ),
               HeightSpacer(
@@ -178,7 +182,7 @@ class _AddPageState extends ConsumerState<UpdateTask> {
 
                     Navigator.pop(context);
                   } else {
-                    ref.read(checkTaskEntryProvider.notifier).state = false;
+                    // ref.read(checkTaskEntryProvider.notifier).state = false;
 
                     ScaffoldMessenger.of(context).showSnackBar(
                       SnackBar(
@@ -223,7 +227,7 @@ class _AddPageState extends ConsumerState<UpdateTask> {
       repeat: "yes",
     );
     todoCubit.updateItem(task);
-    ref.read(checkTaskEntryProvider.notifier).state = true;
+    // ref.read(checkTaskEntryProvider.notifier).state = true;
   }
 
   bool isContentNotEmpty(
@@ -236,8 +240,10 @@ class _AddPageState extends ConsumerState<UpdateTask> {
   }
 
   void clearSelectedDateAndTime() {
+    setState(() {
     dateCubit.setDate('');
     startTimeCubit.setStart('');
     endTimeCubit.setFinish('');
+     });
   }
 }
